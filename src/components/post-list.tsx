189b58f42e71eb8a38d650f4cc8211b5122e0dcf
kionys <kiwon7271@naver.com>
2024-08-05 04:25:13 +0900

    update: firestore 게시글 전체 조회 api 연결 및 데이터 패칭

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,12 +1,49 @@
-import { useState } from "react";
+import AuthContext from "context/auth-context";
+import { db } from "firebase-app";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 interface PostListProps {
   hasNavigation?: boolean;
 }
 type TabType = "all" | "my";
+export interface PostProps {
+  id?: string;
+  title: string;
+  email: string;
+  summary: string;
+  content: string;
+  createAt: string;
+}
 export default function PostList({ hasNavigation = true }: PostListProps) {
+  const { user } = useContext(AuthContext);
   const [activeTab, setActiveTab] = useState<TabType>("all");
+  const [posts, setPosts] = useState<PostProps[]>([]);
+
+  // posts 컬렉션의 모든 문서를 가져온다.
+  const getPosts = async () => {
+    try {
+      const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(
+        collection(db, "posts"),
+      );
+      querySnapshot.forEach(doc => {
+        const postData = { ...doc.data(), id: doc.id };
+        setPosts(prev => [...prev, postData as PostProps]);
+      });
+    } catch (error) {
+      console.error("Error getting documents: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <>
       {hasNavigation && (
@@ -28,34 +65,31 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
         </div>
       )}
       <div className="post__list">
-        {[...Array(10)].map((e, index) => (
-          <div key={index} className="post__box">
-            <Link to={`/posts/${index}`}>
-              <div className="post__profile-box">
-                <div className="post__profile" />
-                <div className="post__author-name">김기원</div>
-                <div className="post__date">2024-07-31</div>
-              </div>
-              <div className="post__title">게시글 {index}</div>
-              <div className="post__text">
-                What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
-                printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an
-                unknown printer took a galley of type and scrambled it to make a
-                type specimen book. It has survived not only five centuries, but
-                also the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more
-                recently with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsum.
-              </div>
-              <div className="post__utils-box">
-                <div className="post__delete">삭제</div>
-                <div className="post__edit">수정</div>
-              </div>
-            </Link>
-          </div>
-        ))}
+        {posts?.length > 0 ? (
+          posts?.map((post, i) => (
+            <div key={i} className="post__box">
+              <Link to={`/posts/${post?.id}`}>
+                <div className="post__profile-box">
+                  <div className="post__profile" />
+                  <div className="post__author-name">{post?.email}</div>
+                  <div className="post__date">{post?.createAt}</div>
+                </div>
+                <div className="post__title">{post?.title}</div>
+                <div className="post__text">{post?.summary}</div>
+              </Link>
+              {user?.email === post?.email && (
+                <div className="post__utils-box">
+                  <div className="post__delete">삭제</div>
+                  <Link to={`/posts/edit/${post.id}`} className="post__edit">
+                    수정
+                  </Link>
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <div className="post__no-post">게시글이 없습니다.</div>
+        )}
       </div>
     </>
   );
