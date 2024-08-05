cc574df23f96d9258fe5c11b2747d3573e9af18b
kionys <kiwon7271@naver.com>
2024-08-05 12:19:32 +0900

    update: 게시글 수정 폼 컴포넌트 기존(추가)에서 수정 기능 추가 적용

--- src/components/post-form.tsx
+++ src/components/post-form.tsx
@@ -1,11 +1,14 @@
 import AuthContext from "context/auth-context";
 import { db } from "firebase-app";
-import { addDoc, collection } from "firebase/firestore";
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { addDoc, collection, doc, getDoc, updateDoc } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
+import { PostProps } from "./post-list";
 
 export default function PostForm() {
+  const params = useParams();
+  const [post, setPost] = useState<PostProps | null>(null);
   const [title, setTitle] = useState<string>();
   const [summary, setSummary] = useState<string>("");
   const [content, setContent] = useState<string>("");
@@ -29,19 +32,53 @@ export default function PostForm() {
     }
   };
 
+  const getPost = async (id: string) => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+      setPost({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+    }
+  };
+
+  useEffect(() => {
+    if (params?.id) getPost(params?.id);
+  }, [params?.id]);
+
+  useEffect(() => {
+    if (post) {
+      setTitle(post?.title);
+      setSummary(post?.summary);
+      setContent(post?.content);
+    }
+  }, [post]);
+
   const onSubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      // firestore로 데이터 생성
-      await addDoc(collection(db, "posts"), {
-        title: title,
-        summary: summary,
-        content: content,
-        createAt: new Date()?.toLocaleDateString(),
-        email: user?.email,
-      });
-      toast.success("게시글을 생성했습니다.");
-      navigate("/");
+      if (post && post.id) {
+        // 만약 post 데이터가 있다면, firestore로 데이터 수정
+        const postRef = doc(db, "posts", post?.id);
+        await updateDoc(postRef, {
+          title: title,
+          summary: summary,
+          content: content,
+          updateAt: new Date()?.toLocaleDateString(),
+          uid: user?.uid,
+        });
+        toast.success("게시글을 수정했습니다.");
+        navigate(`/posts/${post?.id}`);
+      } else {
+        // firestore로 데이터 생성
+        await addDoc(collection(db, "posts"), {
+          title: title,
+          summary: summary,
+          content: content,
+          createAt: new Date()?.toLocaleDateString(),
+          email: user?.email,
+        });
+        toast.success("게시글을 생성했습니다.");
+        navigate("/");
+      }
     } catch (e: any) {
       toast.error(e?.code);
       console.log(e);
@@ -82,7 +119,11 @@ export default function PostForm() {
         />
       </div>
       <div className="form__block">
-        <input type="submit" value="제출" className="form__btn--submit" />
+        <input
+          type="submit"
+          value={post ? "수정" : "제출"}
+          className="form__btn--submit"
+        />
       </div>
     </form>
   );
